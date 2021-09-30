047cf158566acaedffc4867b5c1a606ac52459e7
rchopinw <rchopin@outlook.com>
2021-09-30 03:43:37 -0400

    gogo

--- median_value_in_sliding_windows.py
+++ median_value_in_sliding_windows.py
@@ -1,3 +1,17 @@
 import heapq
 from collections import defaultdict
 
+
+def maximum_median_value(nums, k):
+    pass
+
+
+class MedianValueInFlow:
+    def __init__(self,):
+        pass
+
+    def put(self, value):
+        pass
+
+    def get(self, ):
+        pass
